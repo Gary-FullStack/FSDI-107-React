3500ede5582c51f25ff9759d067b43b1ea89cd39
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-01-26 22:31:50 -0500

    FSDI-107 Class 1

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+
+import "./footer.css";
+
+function Footer(){
+	return(
+
+		<div className="footer">
+			<hr></hr>
+			<label>Gary M.</label>
+		</div>
+	);
+
+}
+export default Footer;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+
+import "./navbar.css";
+
+function Navbar (){
+	return(
+		<div className="navbar">Menu goes Here</div>
+
+	);
+}
+
+export default Navbar;
+
